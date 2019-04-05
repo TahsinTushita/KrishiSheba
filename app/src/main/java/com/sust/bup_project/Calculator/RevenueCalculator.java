57776d2612c0f09ad0883bf34ccf52b6ac662640
Tushita(2016831026) <kazi26@student.sust.edu>
2019-04-05 15:23:00 +0600

    Fixed

--- app/src/main/java/com/sust/bup_project/Calculator/RevenueCalculator.java
+++ app/src/main/java/com/sust/bup_project/Calculator/RevenueCalculator.java
@@ -1,30 +1,14 @@
 package com.sust.bup_project.Calculator;
 
-
-import android.Manifest;
-import android.app.Activity;
 import android.content.Context;
-import android.content.pm.PackageManager;
-import android.location.Address;
-import android.location.Geocoder;
-import android.location.Location;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.sust.bup_project.R;
-
-import java.io.IOException;
-import java.util.List;
-import java.util.Locale;
 
 public class RevenueCalculator extends Fragment implements LocationFetcher.LocationListener {
     private static Fragment fragment;

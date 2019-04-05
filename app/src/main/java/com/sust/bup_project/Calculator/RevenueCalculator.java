1b7bd295bf5d58ea204075903faaa6efe24f6a01
Ahsan Aziz Ishan <ishan.bn14@gmail.com>
2019-04-05 10:55:49 +0600

    Initial Commit

---
+++ app/src/main/java/com/sust/bup_project/Calculator/RevenueCalculator.java
@@ -0,0 +1,21 @@
+package com.sust.bup_project.Calculator;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.sust.bup_project.R;
+
+public class RevenueCalculator extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_revenue_calculator, container, false);
+    }
+
+}

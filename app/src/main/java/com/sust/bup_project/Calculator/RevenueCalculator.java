a478386e178f982ca23a6d1906d440872735f384
Ahsan Aziz Ishan <ishan.bn14@gmail.com>
2019-04-06 10:08:38 +0600

    update

--- app/src/main/java/com/sust/bup_project/Calculator/RevenueCalculator.java
+++ app/src/main/java/com/sust/bup_project/Calculator/RevenueCalculator.java
@@ -2,6 +2,7 @@ package com.sust.bup_project.Calculator;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -32,7 +33,7 @@ public class RevenueCalculator extends Fragment implements LocationFetcher.Locat
     private TextView locationTextView;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_revenue_calculator, container, false);
         locationTextView = view.findViewById(R.id.location_text_view);
